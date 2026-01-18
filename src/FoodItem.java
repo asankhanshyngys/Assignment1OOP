6950a70ad7c30d74185be2429f0418eaf16fbfd6
asankhanshyngys <255005@astanait.edu.kz>
2026-01-19 02:37:04 +0500

    assignment3 commit

--- src/FoodItem.java
+++
@@ -1,9 +0,0 @@
-public class FoodItem extends MenuItem{
-    public FoodItem(String name, double price){
-        super(name,price,"Food");
-    }
-    @Override
-    public void printInfo(){
-        System.out.println("Food: " + super.toString());
-    }
-}
