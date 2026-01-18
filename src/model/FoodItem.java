872ce6cfa0b086b5c7041d642904615699a59ea1
asankhanshyngys <255005@astanait.edu.kz>
2026-01-19 01:39:02 +0500

    new commit
    # Conflicts:
    #	README.md
    #	src/Main.java
    #	src/model/DrinkItem.java
    #	src/model/FoodItem.java
    #	src/model/MenuItem.java
    #	src/model/Order.java
    #	src/model/Restaurant.java

---
+++ src/model/FoodItem.java
@@ -0,0 +1,11 @@
+package model;
+
+public class FoodItem extends MenuItem{
+    public FoodItem(String name, double price){
+        super(name,price,"Food");
+    }
+    @Override
+    public void printInfo(){
+        System.out.println("Food: " + super.toString());
+    }
+}
